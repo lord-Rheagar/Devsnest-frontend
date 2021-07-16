727c82ad5423dc0b2800155f53d2ffbdb1537beb
lord-Rheager <bodhiswattwac@gmail.com>
2021-07-16 16:25:29 +0530

    THA 19

---
+++ day19/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count1, setCount1]=useState(0)
+  const [count2, setCount2]=useState(0)
+  const [count3, setCount3]=useState(0)
+  const [count4, setCount4]=useState(0)
+  
+  return (
+    <div className="App">
+    <button onClick ={()=>setCount1(count1 +1)}>{count1}</button>
+
+    <button onClick ={()=>setCount2(count2 +1)}>{count2}</button>
+
+    <button onClick ={()=>setCount3(count3 +1)}>{count3}</button>
+
+    <button onClick ={()=>setCount4(count4 +1)}>{count4}</button>
+
+    </div>
+  );
+}
+
+export default App;
