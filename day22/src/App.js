d983993f572dd56b0cf4511abf9d0a9771ecf70e
lord-Rheager <bodhiswattwac@gmail.com>
2021-07-22 17:47:43 +0530

    THA 22

---
+++ day22/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from "./Header.js"
+import MemeGenerator from './MemeGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MemeGenerator />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ day22/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+function Header(){
+
+    return (
+    <Header>
+        <img
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+        alt="Problem?"
+        />
+        <p>Meme Generator</p>
+    </Header>
+    )
+
+}
+
+export default Header
