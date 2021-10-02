5710d00923cce3a751d38e4370e02211b3c22612
lord-Rheager <bodhiswattwac@gmail.com>
2021-10-02 11:13:36 +0530

    THA DAY 35

---
+++ day35/src/components/Stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Stories = () => {
+  return (
+    <div>
+      <div className="stories">
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+        <img src="/img/profile.jpg" alt="profile" />
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
---
+++ day35/src/components/Suggestion.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../actions";
+
+const Suggestion = () => {
+  const user: any = useSelector<any>((state) => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+  return (
+    <div className="suggestion">
+      <div className="userInfo">
+        <img className="option profile" alt="option" src={user.profile}></img>
+        <div className="user">
+          <h1>veer</h1>
+        </div>
+      </div>
+      <div className="suggested">
+        <h2>Suggestions for you.</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Suggestion;
